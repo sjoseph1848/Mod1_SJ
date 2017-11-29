106985000bc65d7777f64abf568a7cb4c198b207
Sam <sjoseph1848@gmail.com>
2017-11-28 23:14:25 -0600

    Add project files.

---
+++ Mod1_SJ/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Declaring the Car() Class
+//This class has 3 properties: Color, Year, and Mileage
+public class Car
+{
+    //Defining properties
+    public string Color { get; set; }
+    public int Year { get; set; }
+    public int Mileage { get; set; }
+}
+
+namespace Mod1_SJ
+{
+    class Test
+    {
+        static void Main()
+        {
+            //Instantiating an object of Car() Class
+            Car Car1 = new Car();
+        }
+    }
+}
+
+
